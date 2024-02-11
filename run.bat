f941f9370b7d3f58ca37e1d0d908d9a23e0bea29
bendikMichal <mbendik97@gmail.com>
2024-02-11 21:47:27 +0100

    script update

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 
-@echo Please enable usb debugging and install
+@echo Please connect your device via usb cable
+@echo Please enable usb debugging and install via usb
 @echo checkout prep.bat for more info
 
 npm start
